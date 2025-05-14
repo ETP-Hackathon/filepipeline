194959a05f3e6cb04b833c8a950eccb0d5317700
Martin Maxime <149656366+cosmos510@users.noreply.github.com>
2025-05-14 23:39:49 +0200

    Implement file upload functionality with Django forms and templates

--- webserv/emify/urls.py
+++ webserv/emify/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('hello-world', views.hello),
-	path('', views.nada)
+	path('', views.nada),
+	path('upload/', views.upload_file, name='upload_file'),
 	
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- webserv/emify/views.py
+++ webserv/emify/views.py
@@ -1,8 +1,20 @@
 from django.urls import path
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import UploadFileForm
 
 def hello(request):
 	return HttpResponse("Hello, world. You're at the polls index.")
 
 def nada(request):
 	return HttpResponse("add /hello-world to the url")
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return render(request, 'upload_success.html')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})

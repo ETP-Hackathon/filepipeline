a5752995789a40b3bd01116331bd7c2466a79a15
Martin Maxime <149656366+cosmos510@users.noreply.github.com>
2025-05-15 02:04:09 +0200

    Add home.html template for file upload interface and update views.py to render it

--- webserv/emify/views.py
+++ webserv/emify/views.py
@@ -2,13 +2,13 @@ from django.urls import path
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import UploadFileForm
-
+import subprocess
 def hello(request):
 	return HttpResponse("Hello, world. You're at the polls index.")
-
+"""
 def nada(request):
 	return HttpResponse("add /hello-world to the url")
-
+"""
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
@@ -18,3 +18,6 @@ def upload_file(request):
     else:
         form = UploadFileForm()
     return render(request, 'upload.html', {'form': form})
+
+def nada(request):
+     return render(request, 'home.html')
